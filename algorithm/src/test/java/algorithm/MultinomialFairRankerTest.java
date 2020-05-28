ca2d4232abe811e468516ea096552fd0abb1801f
Meike Zehlike <meike.zehlike@tu-berlin.de>
2020-05-28 16:09:47 +0200

    created junit testcase

---
+++ algorithm/src/test/java/algorithm/MultinomialFairRankerTest.java
@@ -0,0 +1,14 @@
+package algorithm;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MultinomialFairRankerTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
