bc0a86aa1a40e4434d204c23210ef1c4529920fe
Meike Zehlike <meike.zehlike@tu-berlin.de>
2020-06-11 11:19:18 +0200

    first version of buildFairRanking, some Tests still missing

--- algorithm/src/main/java/algorithm/MultinomialFairRanker.java
+++ algorithm/src/main/java/algorithm/MultinomialFairRanker.java
@@ -4,19 +4,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
 
 public class MultinomialFairRanker {
 
     public enum FairRankingStrategy {
         /**
-         * enum to choose from different ranking strategies --
-         *        MOST_LIKELY = child node with highest mcdf 
-         *        MOST_UNLIKELY = child with lowest mcdf (still valid though) 
-         *        RANDOM = pick random child node
+         * enum to choose from different ranking strategies -- MOST_LIKELY = child node
+         * with highest mcdf MOST_UNLIKELY = child with lowest mcdf (still valid though)
+         * RANDOM = pick random child node
          */
         MOST_LIKELY, MOST_UNLIKELY, RANDOM;
     }
@@ -60,37 +56,40 @@ public class MultinomialFairRanker {
      * @return
      */
     public List<Candidate> buildFairRanking(FairRankingStrategy strategy, Integer length) {
-        // FIXME: Write code for asymmetric case
         this.mTree.isMinimumProportionsSymmetric();
         List<Candidate> result = new ArrayList<>(Arrays.asList(new Candidate[length]));
-
+        List<Integer> parent = this.mTree.getRoot();
         for (int k = 0; k < length; k++) {
-            // check how many candidates of each group is needed at least at current k
-            // as their may be different possibilities, take the one with the according
-            // strategy
-            // if the tree is symmetric, the selected node has a mirror with the exact same
-            // mcdf and
-            List<Integer> mNode = getTheNode(strategy, k+1);
-            // is therefore equally likely to be selected. Pick one of these at random
+            // assert that there are actually any candidates left to be ranked
+            if (!anyCandidatesLeft()) {
+                throw new IllegalArgumentException("Not enough candidates to create ranking of size " + length);
+            }
+            // get the next mNode in mTree, that maps the strategy
+            // as their may be several possibilities when the tree is symmetric, take the one most fitting with the
+            // given strategy
+            List<Integer> mNode = this.mTree.getCorrectChildNode(strategy, k + 1, parent);
             boolean candidateAdded = false;
-            // start from index 1, because first entry in node is current position
+            // start from index 1, because first entry in mNode is current ranking position
             for (int groupID = 1; groupID < mNode.size(); groupID++) {
+                // check how many candidates of each group are needed at least at current k
                 Integer expected = mNode.get(groupID);
                 Integer actual = this.numberOfCandidatesInResultPerGroup.get(groupID);
                 if (expected > actual) {
                     // check if multinomial ranked group fairness criteria is met,
                     // if not add candidate from group where is missing
-                    // always use first candidate, because this is the best in terms of scores
+                    // always use first candidate, because this is the best one in terms of scores
                     try {
                         Candidate candidate = groupLists.get(groupID).get(0);
                         result.set(k, candidate);
                         candidateAdded = true;
-                        this.numberOfCandidatesInResultPerGroup.put(groupID, actual+1);
+                        this.numberOfCandidatesInResultPerGroup.put(groupID, actual + 1);
                         // do not put the same candidate twice, therefore remove from pool
                         groupLists.get(groupID).remove(0);
-                        // no need to add candidates twice at the same position
+                        // no need to add more than one candidates at the same k
                         break;
                     } catch (IndexOutOfBoundsException e) {
+                        // if a candidate from groupID should be put, but no more candidates from this
+                        // group are available, a fair ranking cannot be created
                         throw new IllegalArgumentException("Not enough candidates in group " + groupID
                                 + " to create ranking with parameters k=" + this.mTree.getK() + ", p="
                                 + this.mTree.getP() + ", and alpha=" + this.mTree.getAlpha());
@@ -100,71 +99,35 @@ public class MultinomialFairRanker {
             if (!candidateAdded) {
                 // if ranked group fairness condition is met, add the candidate with the highest
                 // score
-                // FIXME: What to do if one list is already empty?
-                Candidate bestCandidate = groupLists.get(0).get(0);
-                for (int groupID = 1; groupID < mNode.size(); groupID++) {
-                    if (bestCandidate.getScore() < groupLists.get(groupID).get(0).getScore()) {
+                Candidate bestCandidate = new Candidate(0.0, 0);
+                for (int groupID = 0; groupID < mNode.size(); groupID++) {
+                    double currentGroupsBestScore = 0.0;
+                    try {
+                        currentGroupsBestScore = groupLists.get(groupID).get(0).getScore();
+                    } catch (IndexOutOfBoundsException e) {
+                        // we have to handle the case that one group list is already empty,
+                        // because we have ranked all its candidates
+                        System.out.println("All candidates of group " + groupID + " are already ranked.");
+                        continue;
+                    }
+                    if (bestCandidate.getScore() < currentGroupsBestScore) {
                         bestCandidate = groupLists.get(groupID).get(0);
                     }
                 }
                 result.set(k, bestCandidate);
                 groupLists.get(bestCandidate.getGroup()).remove(0);
-
             }
-
-        }
-
-        return null;
-    }
-
-    protected List<Integer> getTheNode(FairRankingStrategy strategy, int currentPosition) {
-        /**
-         * from all possible mNodes at this layer, returns the node that fits the
-         * defined strategy
-         * 
-         * @param strategy: enum to choose from different ranking strategies --
-         *        MOST_LIKELY = child node with highest mcdf 
-         *        MOST_UNLIKELY = child with lowest mcdf (still valid though) 
-         *        RANDOM = pick random child node
-         */
-        HashSet<List<Integer>> mNodesAtPosition = this.mTree.getAllNodesOfLevel(currentPosition);
-        MCDFCache mcdfCache = this.mTree.getMcdfCache();
-
-        // get any node of this layer in the mTree to initialize
-        List<Integer> result = mNodesAtPosition.iterator().next();
-
-        switch (strategy) {
-        case MOST_LIKELY:
-            Double highestMCDF = 0.0;
-            // find the likeliest node
-            for (List<Integer> mNode : mNodesAtPosition) {
-                if (mcdfCache.mcdf(mNode) > highestMCDF) {
-                    result = mNode;
-                }
-            }
-            break;
-        case MOST_UNLIKELY:
-            Double lowestMCDF = 1.0;
-            // find the unlikeliest node
-            for (List<Integer> mNode : mNodesAtPosition) {
-                if (mcdfCache.mcdf(mNode) < lowestMCDF) {
-                    result = mNode;
-                }
-            }
-            break;
-        case RANDOM:
-            // from all elements in set, pick one at random from uniform distributed
-            // randomness
-            int randomIndex = new Random().nextInt(mNodesAtPosition.size());
-            Iterator<List<Integer>> iter = mNodesAtPosition.iterator();
-            for (int i = 0; i < randomIndex; i++) {
-                iter.next();
-            }
-            result = iter.next();
-            break;
-        default:
-            throw new IllegalArgumentException("strategy must be either MOST_LIKELY, MOST_UNLIKELY or RANDOM");
+            parent = mNode;
         }
         return result;
+    }
+
+    private boolean anyCandidatesLeft() {
+        for (List<Candidate> groupList : groupLists.values()) {
+            if (!groupList.isEmpty()) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- algorithm/src/test/java/algorithm/MultinomialFairRankerTest.java
+++ algorithm/src/test/java/algorithm/MultinomialFairRankerTest.java
@@ -1,13 +1,10 @@
 package algorithm;
 
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -47,18 +44,23 @@ public class MultinomialFairRankerTest {
     public void setup() {
         
         this.unfairRanking = new ArrayList<>();
-        unfairRanking.add(0, new Candidate(10.0, 0));
-        unfairRanking.add(1, new Candidate(9.0, 0));
-        unfairRanking.add(2, new Candidate(8.0, 0));
-        unfairRanking.add(3, new Candidate(7.0, 0));
+        //group 0
+        unfairRanking.add(new Candidate(11.0, 0));
+        unfairRanking.add(new Candidate(10.0, 0));
+        unfairRanking.add(new Candidate(9.0, 0));
+        unfairRanking.add(new Candidate(8.0, 0));
+        unfairRanking.add(new Candidate(7.0, 0));
 
-        unfairRanking.add(4, new Candidate(6.0, 1));
-        unfairRanking.add(5, new Candidate(5.0, 1));
-        unfairRanking.add(6, new Candidate(4.0, 1));
+        //group 1
+        unfairRanking.add(new Candidate(6.0, 1));
+        unfairRanking.add(new Candidate(5.0, 1));
+        unfairRanking.add(new Candidate(4.0, 1));
 
-        unfairRanking.add(7, new Candidate(3.0, 2));
-        unfairRanking.add(8, new Candidate(2.0, 2));
-        
+        //group 2
+        unfairRanking.add(new Candidate(3.0, 2));
+        unfairRanking.add(new Candidate(2.0, 2));
+        unfairRanking.add(new Candidate(1.0, 2));
+                
         this.rankerSymmetricTree = new MultinomialFairRanker(9,
                                                              new double[] {1.0/3.0, 1.0/3.0, 1.0/3.0},
                                                              0.1, 
@@ -84,14 +86,14 @@ public class MultinomialFairRankerTest {
         List<Candidate> expectedRankingSymmetric = new ArrayList<>();
         expectedRankingSymmetric.add(unfairRanking.get(0));
         expectedRankingSymmetric.add(unfairRanking.get(1));
-        expectedRankingSymmetric.add(unfairRanking.get(4));
-        expectedRankingSymmetric.add(unfairRanking.get(7));
-        expectedRankingSymmetric.add(unfairRanking.get(2));
         expectedRankingSymmetric.add(unfairRanking.get(5));
         expectedRankingSymmetric.add(unfairRanking.get(8));
-        expectedRankingSymmetric.add(unfairRanking.get(3));
+        expectedRankingSymmetric.add(unfairRanking.get(2));
         expectedRankingSymmetric.add(unfairRanking.get(6));
-
+        expectedRankingSymmetric.add(unfairRanking.get(9));
+        expectedRankingSymmetric.add(unfairRanking.get(3));
+        expectedRankingSymmetric.add(unfairRanking.get(7));
+        
         List<Candidate> actualRankingSymmetric = this.rankerSymmetricTree.buildFairRanking(FairRankingStrategy.MOST_LIKELY, 9);
 
         assertArrayEquals(actualRankingSymmetric.toArray(),expectedRankingSymmetric.toArray());
@@ -103,16 +105,20 @@ public class MultinomialFairRankerTest {
         List<Candidate> expectedRankingAsymmetric = new ArrayList<>();
         expectedRankingAsymmetric.add(unfairRanking.get(0));
         expectedRankingAsymmetric.add(unfairRanking.get(1));
-        expectedRankingAsymmetric.add(unfairRanking.get(4));
-        expectedRankingAsymmetric.add(unfairRanking.get(7));
+        expectedRankingAsymmetric.add(unfairRanking.get(8));
         expectedRankingAsymmetric.add(unfairRanking.get(2));
         expectedRankingAsymmetric.add(unfairRanking.get(5));
-        expectedRankingAsymmetric.add(unfairRanking.get(8));
         expectedRankingAsymmetric.add(unfairRanking.get(3));
+        expectedRankingAsymmetric.add(unfairRanking.get(9));
+        expectedRankingAsymmetric.add(unfairRanking.get(4));
+        expectedRankingAsymmetric.add(unfairRanking.get(10));
         expectedRankingAsymmetric.add(unfairRanking.get(6));
+        
+        System.out.println(expectedRankingAsymmetric.toString());
 
         List<Candidate> actualRankingAsymmetric = this.rankerAsymmetricTree.buildFairRanking(FairRankingStrategy.MOST_LIKELY, 10);
-
+        
+        System.out.println(actualRankingAsymmetric);
         assertArrayEquals(actualRankingAsymmetric.toArray(),expectedRankingAsymmetric.toArray());
     }
     
@@ -123,15 +129,15 @@ public class MultinomialFairRankerTest {
     }
     
     @Test
-    public void testBuildFairRanking_MirroredNodes() {
+    public void testBuildFairRanking_NotEnoughCandidates() {
         fail("Not yet implemented");
     }
     
     @Test
-    public void testGetTheNode() {
-        List<Integer> expected = Arrays.stream(new int[] {4, 1, 1}).boxed().collect(Collectors.toList());
-        List<Integer> actual = this.rankerSymmetricTree.getTheNode(FairRankingStrategy.MOST_LIKELY, 4);
-        assertEquals(expected, actual);
+    public void testBuildFairRanking_MirroredNodes() {
+        fail("Not yet implemented");
     }
+    
+
 
 }
