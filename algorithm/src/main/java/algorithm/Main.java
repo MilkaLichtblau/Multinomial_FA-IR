1916120b69af31752fe9ae253fd95b6d965f7154
Tom <tom.suehr@googlemail.com>
2020-06-18 13:12:26 +0200

    added directory checking

--- algorithm/src/main/java/algorithm/Main.java
+++ algorithm/src/main/java/algorithm/Main.java
@@ -1,10 +1,6 @@
 package algorithm;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.util.ArrayList;
@@ -124,9 +120,9 @@ public class Main {
 
     /**
      * ***FailProb Experiment***
-     * args structure: kMax p1 p2 ... pn alpha PATH\FileName
+     * args structure: kMax p1 p2 ... pn alpha PATH/FileName
      * Creates unadjusted mTrees with parameters k=5 to kMax p1..pn alpha and stores their failprobability in the specified file in csv format.
-     * Example for failProb experiment: java -jar algorithm.jar failprob 150 p0.3 p0.2 p0.5 0.05 PATH\TO\EXPERIMENT\FILE\OUTPUT\failProbExperiment1.csv
+     * Example for failProb experiment: java -jar algorithm.jar failprob 150 p0.3 p0.2 p0.5 0.05 PATH/TO/EXPERIMENT/FILE/OUTPUT/failProbExperiment1.csv
      *
      * ***Adjust***
      * args structure k p1 ... pn alpha
@@ -134,35 +130,40 @@ public class Main {
      * Example for adjust experiment: java -jar algorithm.jar adjust 100 p1.0/3.0 p1.0/3.0 p1.0/3.0 0.1
      *
      * ***Data Experiment***
-     * Example for data experiment: java -jar algorithm.jar data ....
+     * args structure data path/to/input/file k p1...pn alpha path/to/output/file (without file extension)
+     * Example for data experiment: java -jar algorithm.jar data ../experiments/dataExperiments/data/COMPAS/compas_race_java.csv 500 0.5,0.5 0.1 ../experiments/dataExperiments/results/COMPAS/compas_race
      */
-    public static void main(String[] args) {
-        try {
-            if (args[0].equals("failprob")) {
-                parseParametersForExperiment(args);
-            }
-            if (args[0].equals("adjust")) {
-                parseParametersForExperiment(args);
-                System.out.println("adjusted MTree stored in ..\\storage\\mtree\\..");
-            }
-            if (args[0].equals("data")) {
-                String datafile = args[1];
-                int k = Integer.parseInt(args[2]);
-                String[] pStringArray = args[3].split(",");
-                double[] p = Arrays.stream(pStringArray)
-                        .mapToDouble(Double::parseDouble)
-                        .toArray();
-                double alpha = Double.parseDouble(args[4]);
-                String resultFilename = args[5];
-                System.out.println("Working Directory = " + System.getProperty("user.dir"));
+    public static void main(String[] args) throws FileNotFoundException {
+        if (Serializer.checkIfStorageDirectoriesExist()) {
+            try {
+                if (args[0].equals("failprob")) {
+                    parseParametersForExperiment(args);
+                }
+                if (args[0].equals("adjust")) {
+                    parseParametersForExperiment(args);
+                    System.out.println("adjusted MTree stored in ../storage/mtree/..");
+                }
+                if (args[0].equals("data")) {
+                    String datafile = args[1];
+                    int k = Integer.parseInt(args[2]);
+                    String[] pStringArray = args[3].split(",");
+                    double[] p = Arrays.stream(pStringArray)
+                            .mapToDouble(Double::parseDouble)
+                            .toArray();
+                    double alpha = Double.parseDouble(args[4]);
+                    String resultFilename = args[5];
+                    System.out.println("Working Directory = " + System.getProperty("user.dir"));
 
-                Main.prepareDataExperiments(datafile, ",", true);
-                MultinomialFairRanker ranker = new MultinomialFairRanker(k, p, alpha, true, unfairRanking);
-                Main.fairRanking = ranker.buildFairRanking(FairRankingStrategy.MOST_LIKELY, k);
-                Main.writeRankingsToCSV(resultFilename);
+                    Main.prepareDataExperiments(datafile, ",", true);
+                    MultinomialFairRanker ranker = new MultinomialFairRanker(k, p, alpha, true, unfairRanking);
+                    Main.fairRanking = ranker.buildFairRanking(FairRankingStrategy.MOST_LIKELY, k);
+                    Main.writeRankingsToCSV(resultFilename);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        }else{
+            throw new FileNotFoundException("Directories ./storage/mtree and ./storage/mcdfcache required.");
         }
     }
 }
