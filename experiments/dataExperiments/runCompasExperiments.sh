ba199943ecc189661482da798787cc438a57f6ac
Meike Zehlike <meike.zehlike@tu-berlin.de>
2020-06-22 15:02:57 +0200

    bash scripts for Compas and German Credit

--- experiments/dataExperiments/runCompasExperiments.sh
+++ experiments/dataExperiments/runCompasExperiments.sh
@@ -1,9 +1,56 @@
 #!/bin/bash
 
 GIT_ROOT="$(git rev-parse --show-toplevel)"
-PATH_TO_JAR=$GIT_ROOT/algorithm
+PATH_TO_FAIR_JAR=$GIT_ROOT/algorithm
+PATH_TO_CFA_ALGORITHM=$GIT_ROOT/experiments/dataExperiments/cfa/src
 PATH_TO_COMPAS_DATA=$GIT_ROOT/experiments/dataExperiments/data/COMPAS
 PATH_TO_COMPAS_RESULTS=$GIT_ROOT/experiments/dataExperiments/results/COMPAS/rankings
 
-cd $PATH_TO_JAR
-java -jar MultinomialFair.jar data $PATH_TO_COMPAS_DATA/compas_race_java.csv 500 0.5,0.5 0.1 $PATH_TO_COMPAS_RESULTS/compas_race
+
+###################################################################
+# AGE (younger than 25 (P1), 25-45 (NP), older than 45 (P2)), RACE (White, Non-White)
+#####################################################################
+cd $PATH_TO_FAIR_JAR
+
+# p is same percentages as in dataset
+java -jar Multinomial_FA-IR.jar data $PATH_TO_COMPAS_DATA/compas_ageRace_java.csv 500 0.18,0.11,0.1,0.39,0.06,0.16 0.1 $PATH_TO_COMPAS_RESULTS/compas_ageRace
+
+# p is all the same
+java -jar Multinomial_FA-IR.jar data $PATH_TO_COMPAS_DATA/compas_ageRace_java.csv 500 0.16,0.16,0.16,0.16,0.16,0.16 0.1 $PATH_TO_COMPAS_RESULTS/compas_ageRace
+
+cd $PATH_TO_CFA_ALGORITHM
+#baseline
+python3 main.py compas_ageRace 0,1 0.01 0,0,0,0,0,0 $PATH_TO_COMPAS_RESULTS/compas_ageRace_CFA_theta=0.csv
+python3 main.py compas_ageRace 0,1 0.01 1 1,1,1,1,1,1 $PATH_TO_COMPAS_RESULTS/compas_ageRace_CFA_theta=1.csv
+
+#################################################ü
+# SEX (male, female), RACE (White, Non-White)
+###################################################
+cd $PATH_TO_FAIR_JAR
+# p is same percentages as in dataset
+java -jar Multinomial_FA-IR.jar data $PATH_TO_COMPAS_DATA/compas_sexRace_java.csv 500 0.26,0.55,0.08,0.11 0.1 $PATH_TO_COMPAS_RESULTS/compas_sexRace
+
+#p is all the same
+java -jar Multinomial_FA-IR.jar data $PATH_TO_COMPAS_DATA/compas_sexRace_java.csv 500 0.25,0.25,0.25,0.25 0.1 $PATH_TO_COMPAS_RESULTS/compas_sexRace
+
+cd $PATH_TO_CFA_ALGORITHM
+#baseline
+python3 main.py compas_sexRace 0,1 0.01 0,0,0,0 $PATH_TO_COMPAS_RESULTS/compas_sexRace_CFA_theta=0.csv
+python3 main.py --run compas_sexRace 0,1 0.01 1 1,1,1,1 $PATH_TO_COMPAS_RESULTS/compas_sexRace_CFA_theta=1.csv
+
+###########################################
+# SEX, AGE
+###########################################
+cd $PATH_TO_FAIR_JAR
+# p is same percentages as in dataset
+java -jar Multinomial_FA-IR.jar data $PATH_TO_COMPAS_DATA/compas_sexAge_java.csv 500 0.46,0.17,0.04,0.11,0.18,0.04 0.1 $PATH_TO_COMPAS_RESULTS/compas_sexAge
+
+#p is all the same
+java -jar Multinomial_FA-IR.jar data $PATH_TO_COMPAS_DATA/compas_sexAge_java.csv 500 0.16,0.16,0.16,0.16,0.16,0.16 0.1 $PATH_TO_COMPAS_RESULTS/compas_sexAge
+
+cd $PATH_TO_CFA_ALGORITHM
+#baseline
+python3 main.py compas_sexAge 0,1 0.01 0,0,0,0,0,0 $PATH_TO_COMPAS_RESULTS/compas_sexAge_CFA_theta=0.csv
+python3 main.py compas_sexAge 0,1 0.01 1 1,1,1,1,1,1 $PATH_TO_COMPAS_RESULTS/compas_sexAge_CFA_theta=1.csv
+
+
--- experiments/dataExperiments/runGermanCreditExperiments.sh
+++ experiments/dataExperiments/runGermanCreditExperiments.sh
@@ -5,5 +5,14 @@ PATH_TO_JAR=$GIT_ROOT/algorithm
 PATH_TO_GERMAN_CREDIT_DATA=$GIT_ROOT/experiments/dataExperiments/data/GermanCredit
 PATH_TO_GERMAN_CREDIT_RESULTS=$GIT_ROOT/experiments/dataExperiments/results/GermanCredit/rankings
 
+# SEX (Male, Female), AGE (100 youngest and 100 oldest are protected)
 cd $PATH_TO_JAR
-java -jar MultinomialFair.jar data $PATH_TO_GERMAN_CREDIT_DATA/germanCredit_age_java.csv 500 0.5,0.5 0.1 $PATH_TO_GERMAN_CREDIT_RESULTS/germanCredit_age
+
+# p matches percentages of groups in dataset (statistical parity)
+java -jar MultinomialFair.jar data $PATH_TO_GERMAN_CREDIT_DATA/germanCredit_sexAge_java.csv 500 0.58,0.22,0.06,0.06,0.04,0.03 0.1 $PATH_TO_GERMAN_CREDIT_RESULTS/germanCredit_sexAge
+
+# p is equal for all
+java -jar MultinomialFair.jar data $PATH_TO_GERMAN_CREDIT_DATA/germanCredit_sexAge_java.csv 500 0.16,0.16,0.16,0.16,0.16,0.16 0.1 $PATH_TO_GERMAN_CREDIT_RESULTS/germanCredit_sexAge
+
+# p is something in between
+java -jar MultinomialFair.jar data $PATH_TO_GERMAN_CREDIT_DATA/germanCredit_sexAge_java.csv 500 0.37,0.19,0.12,0.12,0.104,0.096 0.1 $PATH_TO_GERMAN_CREDIT_RESULTS/germanCredit_sexAge
