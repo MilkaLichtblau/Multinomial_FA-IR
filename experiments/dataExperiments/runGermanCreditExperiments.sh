089d6829b747c08d87fb4c9adc954c2f7a112108
Meike Zehlike <meike.zehlike@tu-berlin.de>
2020-06-23 10:54:37 +0200

    added baseline experiments

--- experiments/dataExperiments/runGermanCreditExperiments.sh
+++ experiments/dataExperiments/runGermanCreditExperiments.sh
@@ -9,13 +9,13 @@ PATH_TO_GERMAN_CREDIT_RESULTS=$GIT_ROOT/experiments/dataExperiments/results/Germ
 # SEX (Male, Female), AGE (100 youngest and 100 oldest are protected)
 cd $PATH_TO_JAR
 # p matches percentages of groups in dataset (statistical parity)
-java -jar MultinomialFair.jar data $PATH_TO_GERMAN_CREDIT_DATA/germanCredit_sexAge_java.csv 500 0.58,0.22,0.06,0.06,0.04,0.03 0.1 $PATH_TO_GERMAN_CREDIT_RESULTS/germanCredit_sexAge
+java -jar Multinomial_FA-IR.jar data $PATH_TO_GERMAN_CREDIT_DATA/germanCredit_sexAge_java.csv 500 0.58,0.22,0.06,0.06,0.04,0.03 0.1 $PATH_TO_GERMAN_CREDIT_RESULTS/germanCredit_sexAge
 
 # p is equal for all
-java -jar MultinomialFair.jar data $PATH_TO_GERMAN_CREDIT_DATA/germanCredit_sexAge_java.csv 500 0.16,0.16,0.16,0.16,0.16,0.16 0.1 $PATH_TO_GERMAN_CREDIT_RESULTS/germanCredit_sexAge
+java -jar Multinomial_FA-IR.jar data $PATH_TO_GERMAN_CREDIT_DATA/germanCredit_sexAge_java.csv 500 0.16,0.16,0.16,0.16,0.16,0.16 0.1 $PATH_TO_GERMAN_CREDIT_RESULTS/germanCredit_sexAge
 
 # p is something in between
-java -jar MultinomialFair.jar data $PATH_TO_GERMAN_CREDIT_DATA/germanCredit_sexAge_java.csv 500 0.37,0.19,0.12,0.12,0.104,0.096 0.1 $PATH_TO_GERMAN_CREDIT_RESULTS/germanCredit_sexAge
+java -jar Multinomial_FA-IR.jar data $PATH_TO_GERMAN_CREDIT_DATA/germanCredit_sexAge_java.csv 500 0.37,0.19,0.12,0.12,0.104,0.096 0.1 $PATH_TO_GERMAN_CREDIT_RESULTS/germanCredit_sexAge
 
 cd $PATH_TO_CFA_ALGORITHM
 python3 main.py germanCredit_sexAge 0.1 0,0,0,0,0,0 $PATH_TO_GERMAN_CREDIT_RESULTS/germanCredit_sexAge_CFA_theta=0.csv
--- experiments/dataExperiments/runLSATExperiments.sh
+++ experiments/dataExperiments/runLSATExperiments.sh
@@ -9,10 +9,10 @@ PATH_TO_LSAT_RESULTS=$GIT_ROOT/experiments/dataExperiments/results/LSAT/rankings
 # SEX (Male, Female), RACE (White, Non-White)
 cd $PATH_TO_JAR
 # p matches percentages of groups in dataset (statistical parity)
-java -jar MultinomialFair.jar data $PATH_TO_LSAT_DATA/LSAT_sexRace_java.csv 2000 0.49,0.35,0.084,0.076 0.1 $PATH_TO_LSAT_RESULTS/LSAT_sexRace
+java -jar Multinomial_FA-IR.jar data $PATH_TO_LSAT_DATA/LSAT_sexRace_java.csv 2000 0.49,0.35,0.084,0.076 0.1 $PATH_TO_LSAT_RESULTS/LSAT_sexRace
 
 # p is equal for all
-java -jar MultinomialFair.jar data $PATH_TO_LSAT_DATA/LSAT_sexRace_java.csv 2000 0.25,0.25,0.25,0.25 0.1 $PATH_TO_LSAT_RESULTS/LSAT_sexRace
+java -jar Multinomial_FA-IR.jar data $PATH_TO_LSAT_DATA/LSAT_sexRace_java.csv 2000 0.25,0.25,0.25,0.25 0.1 $PATH_TO_LSAT_RESULTS/LSAT_sexRace
 
 cd $PATH_TO_CFA_ALGORITHM
 python3 main.py LSAT_sexRace 0.1 0,0,0,0,0,0 $PATH_TO_LSAT_RESULTS/LSAT_sexRace_CFA_theta=0.csv
