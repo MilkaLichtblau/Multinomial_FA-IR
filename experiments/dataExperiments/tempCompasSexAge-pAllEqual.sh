b99bc56a4fb0d7f24edb5716de3ed554fc3cc1d5
Meike Zehlike <meike.zehlike@tu-berlin.de>
2020-07-08 13:25:46 +0200

    corrected p vectors

--- experiments/dataExperiments/tempCompasSexAge-pAllEqual.sh
+++ experiments/dataExperiments/tempCompasSexAge-pAllEqual.sh
@@ -16,7 +16,7 @@ cd $PATH_TO_FAIR_JAR
 
 #p is all the same
 echo "Multi FAIR with p all equal"
-java -jar Multinomial_FA-IR.jar data $PATH_TO_COMPAS_DATA/compas_sexAge_java.csv 500 0.16,0.16,0.16,0.16,0.16,0.16 0.1 $PATH_TO_COMPAS_RESULTS/sexAge/compas_sexAge
+java -jar Multinomial_FA-IR.jar data $PATH_TO_COMPAS_DATA/compas_sexAge_java.csv 500 1.0/6.0,1.0/6.0,1.0/6.0,1.0/6.0,1.0/6.0,1.0/6.0 0.1 $PATH_TO_COMPAS_RESULTS/sexAge/compas_sexAge
 
 cd $PATH_TO_CFA_ALGORITHM
 #baseline
