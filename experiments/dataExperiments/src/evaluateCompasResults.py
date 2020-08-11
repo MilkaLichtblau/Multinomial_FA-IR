3003ebba9b94e077758a9c75bb96c648b15990ef
Meike Zehlike <meike.zehlike@tu-berlin.de>
2020-08-11 17:54:05 +0200

    redid baseline experiments with theta=05

--- experiments/dataExperiments/src/evaluateCompasResults.py
+++ experiments/dataExperiments/src/evaluateCompasResults.py
@@ -7,7 +7,7 @@ Created on Jul 15, 2020
 import pandas as pd
 import glob
 import sklearn.metrics
-from src.util import selectionUtilityLossPerGroup, orderingUtilityLossPerGroup
+from src.util import selectionUtilityLossPerGroup, orderingUtilityLossPerGroup, ndcgLoss, ndcg_score
 
 COMPAS_RANKINGS_DIR = "../results/COMPAS/rankings/"
 EXPERIMENT_NAMES = ["race", "ageRace", "sexAge", "sexRace"]
@@ -19,6 +19,8 @@ def main():
         allFairRankingFilenames = glob.glob(COMPAS_RANKINGS_DIR + experiment + "/" + "*_fair.csv")
         allUnfairRankingFilenames = glob.glob(COMPAS_RANKINGS_DIR + experiment + "/" + "*_unfair.csv")
         allRemainingFilenames = glob.glob(COMPAS_RANKINGS_DIR + experiment + "/" + "*_remaining.csv")
+        CFAHalfFilename = glob.glob(COMPAS_RANKINGS_DIR + experiment + "/" + "*_theta=05.csv")
+        CFAOneFilename = glob.glob(COMPAS_RANKINGS_DIR + experiment + "/" + "*_theta=1.csv")
         # get all files with corresponding k
         for fairRankingFilename in allFairRankingFilenames:
             fairRanking = pd.read_csv(fairRankingFilename, header=0, skipinitialspace=True)
@@ -33,15 +35,42 @@ def main():
                                            header=0,
                                            skipinitialspace=True)
 
-            result = pd.DataFrame()
-            print(fairRanking.columns)
-            result["group"] = fairRanking['group'].unique()
-            result = selectionUtilityLossPerGroup(remainingRanking, fairRanking, result)
-            result = orderingUtilityLossPerGroup(colorblindRanking, fairRanking, result)
-            result["ndcgLoss"] = 1 - sklearn.metrics.ndcg_score(colorblindRanking.head(len(fairRanking)), fairRanking)
+            # load baseline rankings
+            thetaHalfRanking = pd.read_csv(CFAHalfFilename, header=0)
+            thetaOneRanking = pd.read_csv(CFAOneFilename, header=0)
 
-    thetaZeroRanking = pd.read_csv(COMPAS_RANKINGS_DIR + "race/compas_race_CFA_theta=0.csv", header=0)
-    thetaOneRanking = pd.read_csv(COMPAS_RANKINGS_DIR + "race/compas_race_CFA_theta=1.csv", header=0)
+            # eval for Multinomial FA*IR
+            multi_fair_result = pd.DataFrame()
+            multi_fair_result["group"] = fairRanking['group'].unique()
+            multi_fair_result = selectionUtilityLossPerGroup(remainingRanking, fairRanking, multi_fair_result)
+            multi_fair_result = orderingUtilityLossPerGroup(colorblindRanking, fairRanking, multi_fair_result)
+            multi_fair_result["ndcgLoss"] = 1 - ndcg_score(colorblindRanking["score"].to_numpy(),
+                                                fairRanking["score"].to_numpy(),
+                                                k=len(fairRanking))
+            multi_fair_result["exposureGain"] = 0.0
+
+            # eval for CFA algorithm with theta=0.5
+            kay = len(fairRanking)
+            n = len(thetaHalfRanking) - kay
+            cfaHalf_result = pd.DataFrame()
+            cfaHalf_result["group"] = fairRanking['group'].unique()
+            cfaHalf_result = selectionUtilityLossPerGroup(thetaHalfRanking.tail(n), thetaHalfRanking.head(kay), cfaHalf_result)
+            cfaHalf_result = orderingUtilityLossPerGroup(colorblindRanking, thetaHalfRanking.head(kay), cfaHalf_result)
+            cfaHalf_result["ndcgLoss"] = 1 - ndcg_score(colorblindRanking["score"].to_numpy(),
+                                                thetaHalfRanking.head(kay)["score"].to_numpy(),
+                                                k=kay)
+            cfaHalf_result["exposureGain"] = 0.0
+
+            # eval for CFA algorithm with theta=1
+            n = len(thetaOneRanking) - kay
+            cfaOne_result = pd.DataFrame()
+            cfaOne_result["group"] = fairRanking['group'].unique()
+            cfaOne_result = selectionUtilityLossPerGroup(thetaOneRanking.tail(n), thetaOneRanking.head(kay), cfaOne_result)
+            cfaOne_result = orderingUtilityLossPerGroup(colorblindRanking, thetaOneRanking.head(kay), cfaOne_result)
+            cfaOne_result["ndcgLoss"] = 1 - ndcg_score(colorblindRanking["score"].to_numpy(),
+                                                thetaOneRanking.head(kay)["score"].to_numpy(),
+                                                k=kay)
+            cfaOne_result["exposureGain"] = 0.0
 
 
 if __name__ == '__main__':
--- experiments/dataExperiments/tempGermanCreditSexAge-baseline.sh
+++ experiments/dataExperiments/tempGermanCreditSexAge-baseline.sh
@@ -11,4 +11,4 @@ echo "Baseline with CFA theta = 0.5"
 cd $PATH_TO_CFA_ALGORITHM
 python3 main.py germanCredit_sexAge 0.1 0.5,0.5,0.5,0.5,0.5,0.5 $PATH_TO_GERMAN_CREDIT_RESULTS/germanCredit_sexAge_CFA_theta=05.csv
 echo "Baseline with CFA theta = 1"
-python3 main.py germanCredit_sexAge 0.1 1 1,1,1,1,1,1 $PATH_TO_GERMAN_CREDIT_RESULTS/germanCredit_sexAge_CFA_theta=1.csv
+python3 main.py germanCredit_sexAge 0.1 1,1,1,1,1,1 $PATH_TO_GERMAN_CREDIT_RESULTS/germanCredit_sexAge_CFA_theta=1.csv
