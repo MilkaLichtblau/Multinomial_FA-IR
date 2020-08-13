79d256a1e5f54dc05535cba8e6501aebaf4c4cf6
Meike Zehlike <meike.zehlike@tu-berlin.de>
2020-08-13 12:59:35 +0200

    made k a variable

--- experiments/dataExperiments/tempCompasRace.sh
+++ experiments/dataExperiments/tempCompasRace.sh
@@ -6,6 +6,8 @@ PATH_TO_CFA_ALGORITHM=$GIT_ROOT/experiments/dataExperiments/cfa/src
 PATH_TO_COMPAS_DATA=$GIT_ROOT/experiments/dataExperiments/data/COMPAS
 PATH_TO_COMPAS_RESULTS=$GIT_ROOT/experiments/dataExperiments/results/COMPAS/rankings
 
+K=500
+
 echo "#################################################"
 echo "# STARTING COMPAS RACE EXPERIMENTS"
 echo "#################################################"
@@ -16,11 +18,11 @@ cd $PATH_TO_FAIR_JAR
 
 # p is same percentages as in dataset
 echo "Multi FAIR with p as statistical parity"
-java -jar Multinomial_FA-IR.jar data $PATH_TO_COMPAS_DATA/compas_race_java.csv 1500 0.66,0.34 0.1 $PATH_TO_COMPAS_RESULTS/race/compas_race
+java -jar Multinomial_FA-IR.jar data $PATH_TO_COMPAS_DATA/compas_race_java.csv $K 0.66,0.34 0.1 $PATH_TO_COMPAS_RESULTS/race/compas_race
 
 # p is all the same
 echo "Multi FAIR with p all equal"
-java -jar Multinomial_FA-IR.jar data $PATH_TO_COMPAS_DATA/compas_race_java.csv 1500 0.5,0.5 0.1 $PATH_TO_COMPAS_RESULTS/race/compas_race
+java -jar Multinomial_FA-IR.jar data $PATH_TO_COMPAS_DATA/compas_race_java.csv $K 0.5,0.5 0.1 $PATH_TO_COMPAS_RESULTS/race/compas_race
 
 cd $PATH_TO_CFA_ALGORITHM
 #baseline
