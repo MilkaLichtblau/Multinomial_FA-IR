ec9c57beb56fcf256c509877dbe88883be7a0291
Meike Zehlike <meike.zehlike@tu-berlin.de>
2020-08-20 16:42:13 +0200

    deleted unnecessary experiments and fixed run scripts

--- experiments/dataExperiments/tempCompasAge-pStatisticalParity.sh
+++ experiments/dataExperiments/tempCompasAge-pStatisticalParity.sh
@@ -7,12 +7,15 @@ PATH_TO_COMPAS_DATA=$GIT_ROOT/experiments/dataExperiments/data/COMPAS
 PATH_TO_COMPAS_RESULTS=$GIT_ROOT/experiments/dataExperiments/results/COMPAS/rankings
 
 echo "#################################################"
-echo "# STARTING COMPAS SEX RACE EXPERIMENTS"
+echo "# STARTING COMPAS AGE EXPERIMENTS"
 echo "#################################################"
-#################################################ü
-# SEX (male, female), RACE (White, Non-White)
-###################################################
+###################################################################
+# AGE (younger than 25 (P1), 25-45 (P2), older than 45 (NP)), 
+#####################################################################
 cd $PATH_TO_FAIR_JAR
+
 # p is same percentages as in dataset
 echo "Multi FAIR with p as statistical parity"
-java -jar Multinomial_FA-IR.jar data $PATH_TO_COMPAS_DATA/compas_sexRace_java.csv 500 0.26,0.55,0.08,0.11 0.1 $PATH_TO_COMPAS_RESULTS/sexRace/compas_sexRace
+java -jar Multinomial_FA-IR.jar data $PATH_TO_COMPAS_DATA/compas_ageRace_java.csv 100 0.209,0.573,0.218 0.1 $PATH_TO_COMPAS_RESULTS/ageRace/compas_ageRace
+
+
--- experiments/dataExperiments/tempCompasWorstThree-pAllEqual.sh
+++ experiments/dataExperiments/tempCompasWorstThree-pAllEqual.sh
@@ -14,6 +14,13 @@ echo "# STARTING COMPAS SEX AGE EXPERIMENTS"
 echo "#################################################"
 cd $PATH_TO_FAIR_JAR
 
-# p is same percentages as in dataset
-echo "Multi FAIR with p as statistical parity"
-java -jar Multinomial_FA-IR.jar data $PATH_TO_COMPAS_DATA/compas_sexAge_java.csv 100 0.46,0.17,0.04,0.11,0.18,0.04 0.1 $PATH_TO_COMPAS_RESULTS/sexAge/compas_sexAge
+#p is all the same
+echo "Multi FAIR with p all equal"
+java -jar Multinomial_FA-IR.jar data $PATH_TO_COMPAS_DATA/compas_sexAge_java.csv 100 0.25,0.25,0.25,0.25 0.1 $PATH_TO_COMPAS_RESULTS/sexAge/compas_sexAge
+
+cd $PATH_TO_CFA_ALGORITHM
+#baseline
+echo "Baseline with CFA theta = 0"
+python3 main.py compas_sexAge 0.05 0,0,0,0,0,0 $PATH_TO_COMPAS_RESULTS/sexAge/compas_sexAge_CFA_theta=0.csv
+echo "Baseline with CFA theta = 1"
+python3 main.py compas_sexAge 0.05 1,1,1,1,1,1 $PATH_TO_COMPAS_RESULTS/sexAge/compas_sexAge_CFA_theta=1.csv
