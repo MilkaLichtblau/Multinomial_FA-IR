b976b53caa68eee0fcc71da3871b1bab7204802a
Meike Zehlike <meike.zehlike@tu-berlin.de>
2020-08-20 14:19:55 +0200

    added plots for german credit and lsat

--- experiments/dataExperiments/src/visualize.py
+++ experiments/dataExperiments/src/visualize.py
@@ -33,8 +33,32 @@ def visualizeOrigCompasData():
     plotKDEPerGroup(data, "Negative Recidivism Risk\n(higher is better)", "../data/COMPAS/compas_worstThreeGroups_kde.png", colNames=labels)
 
 
+def visualizeOrigGermanCreditData():
+    data = pd.read_csv("../data/GermanCredit/germanCredit_sex_java.csv", header=0, skipinitialspace=True)
+    labels = {"0": "females",
+              "1": "males"}
+    plotKDEPerGroup(data, "Credit Score\n(higher is better)", "../data/GermanCredit/germanCredit_sex_kde.png", colNames=labels)
+
+    data = pd.read_csv("../data/GermanCredit/germanCredit_age_java.csv", header=0, skipinitialspace=True)
+    labels = {"0": "others",
+              "1": "oldest decile",
+              "2": "youngest decile"}
+    plotKDEPerGroup(data, "Credit Score\n(higher is better)", "../data/GermanCredit/germanCredit_age_kde.png", colNames=labels)
+
+
+def visualizeOrigLSATData():
+    data = pd.read_csv("../data/LSAT/LSAT_sexRace_java.csv", header=0, skipinitialspace=True)
+    labels = {"0": "white males",
+              "1": "white females",
+              "2": "non-white males",
+              "3": "non-white females"}
+    plotKDEPerGroup(data, "LSAT Score\n(higher is better)", "../data/LSAT/LSAT_sexRace_kde.png", colNames=labels)
+
+
 def main():
     visualizeOrigCompasData()
+    visualizeOrigGermanCreditData()
+    visualizeOrigLSATData()
 
 
 if __name__ == '__main__':
