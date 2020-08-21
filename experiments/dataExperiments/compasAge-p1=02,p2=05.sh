3179e51518403fb7cf4df16b5bfb0209abfe3672
Meike Zehlike <meike.zehlike@tu-berlin.de>
2020-08-21 11:47:45 +0200

    bugfix

--- experiments/dataExperiments/compasAge-p1=02,p2=05.sh
+++ experiments/dataExperiments/compasAge-p1=02,p2=05.sh
@@ -9,7 +9,7 @@ PATH_TO_COMPAS_RESULTS=$GIT_ROOT/experiments/dataExperiments/results/COMPAS/rank
 cd $PATH_TO_FAIR_JAR
 
 # p is all the same
-echo "Multi FAIR with p all equal"
+echo "Multi FAIR with p1=0.2 and p2=0.5"
 java -jar Multinomial_FA-IR.jar data $PATH_TO_COMPAS_DATA/compas_age_java.csv 100 0.3,0.2,0.5 0.1 $PATH_TO_COMPAS_RESULTS/age/compas_age
 
 
--- experiments/dataExperiments/compasWorstThree-pAllEqual.sh
+++ experiments/dataExperiments/compasWorstThree-pAllEqual.sh
@@ -7,20 +7,14 @@ PATH_TO_COMPAS_DATA=$GIT_ROOT/experiments/dataExperiments/data/COMPAS
 PATH_TO_COMPAS_RESULTS=$GIT_ROOT/experiments/dataExperiments/results/COMPAS/rankings
 
 ###########################################
-# SEX, AGE
+# WORST THREE
 ###########################################
 echo "#################################################"
-echo "# STARTING COMPAS SEX AGE EXPERIMENTS"
+echo "# STARTING COMPAS WORST THREE EXPERIMENTS"
 echo "#################################################"
 cd $PATH_TO_FAIR_JAR
 
 #p is all the same
 echo "Multi FAIR with p all equal"
-java -jar Multinomial_FA-IR.jar data $PATH_TO_COMPAS_DATA/compas_sexAge_java.csv 100 0.25,0.25,0.25,0.25 0.1 $PATH_TO_COMPAS_RESULTS/sexAge/compas_sexAge
+java -jar Multinomial_FA-IR.jar data $PATH_TO_COMPAS_DATA/compas_worstThreeGroups_java.csv 100 0.25,0.25,0.25,0.25 0.1 $PATH_TO_COMPAS_RESULTS/worstThree/compas_worstThreeGroups
 
-cd $PATH_TO_CFA_ALGORITHM
-#baseline
-echo "Baseline with CFA theta = 0"
-python3 main.py compas_sexAge 0.05 0,0,0,0,0,0 $PATH_TO_COMPAS_RESULTS/sexAge/compas_sexAge_CFA_theta=0.csv
-echo "Baseline with CFA theta = 1"
-python3 main.py compas_sexAge 0.05 1,1,1,1,1,1 $PATH_TO_COMPAS_RESULTS/sexAge/compas_sexAge_CFA_theta=1.csv
