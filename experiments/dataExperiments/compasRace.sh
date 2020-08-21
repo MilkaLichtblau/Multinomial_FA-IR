88f79a80a63af8e794fd6b67d15387a0d86c86eb
Meike Zehlike <meikezehlike@brain24.mpi-sws.org>
2020-08-21 11:54:31 +0200

    bugfix

--- experiments/dataExperiments/compasRace.sh
+++ experiments/dataExperiments/compasRace.sh
@@ -6,7 +6,7 @@ PATH_TO_CFA_ALGORITHM=$GIT_ROOT/experiments/dataExperiments/cfa/src
 PATH_TO_COMPAS_DATA=$GIT_ROOT/experiments/dataExperiments/data/COMPAS
 PATH_TO_COMPAS_RESULTS=$GIT_ROOT/experiments/dataExperiments/results/COMPAS/rankings
 
-K=500
+K=1500
 
 echo "#################################################"
 echo "# STARTING COMPAS RACE EXPERIMENTS"
@@ -24,9 +24,3 @@ java -jar Multinomial_FA-IR.jar data $PATH_TO_COMPAS_DATA/compas_race_java.csv $
 echo "Multi FAIR with p all equal"
 java -jar Multinomial_FA-IR.jar data $PATH_TO_COMPAS_DATA/compas_race_java.csv $K 0.5,0.5 0.1 $PATH_TO_COMPAS_RESULTS/race/compas_race
 
-cd $PATH_TO_CFA_ALGORITHM
-#baseline
-echo "Baseline with CFA theta = 0.5"
-python3 main.py compas_race 0.1 0.5,0.5 $PATH_TO_COMPAS_RESULTS/race/compas_race_CFA_theta=05.csv
-echo "Baseline with CFA theta = 1"
-python3 main.py compas_race 0.1 1,1 $PATH_TO_COMPAS_RESULTS/race/compas_race_CFA_theta=1.csv
--- experiments/dataExperiments/compasWorstThree-pAllEqual.sh
+++ experiments/dataExperiments/compasWorstThree-pAllEqual.sh
@@ -10,17 +10,12 @@ PATH_TO_COMPAS_RESULTS=$GIT_ROOT/experiments/dataExperiments/results/COMPAS/rank
 # SEX, AGE
 ###########################################
 echo "#################################################"
-echo "# STARTING COMPAS SEX AGE EXPERIMENTS"
+echo "# STARTING COMPAS WORST THREE EXPERIMENTS"
 echo "#################################################"
 cd $PATH_TO_FAIR_JAR
 
 #p is all the same
 echo "Multi FAIR with p all equal"
-java -jar Multinomial_FA-IR.jar data $PATH_TO_COMPAS_DATA/compas_sexAge_java.csv 100 0.25,0.25,0.25,0.25 0.1 $PATH_TO_COMPAS_RESULTS/sexAge/compas_sexAge
+java -jar Multinomial_FA-IR.jar data $PATH_TO_COMPAS_DATA/compas_worstThreeGroups_java.csv 100 0.25,0.25,0.25,0.25 0.1 $PATH_TO_COMPAS_RESULTS/worstThree/compas_worstThreeGroups
 
-cd $PATH_TO_CFA_ALGORITHM
-#baseline
-echo "Baseline with CFA theta = 0"
-python3 main.py compas_sexAge 0.05 0,0,0,0,0,0 $PATH_TO_COMPAS_RESULTS/sexAge/compas_sexAge_CFA_theta=0.csv
-echo "Baseline with CFA theta = 1"
-python3 main.py compas_sexAge 0.05 1,1,1,1,1,1 $PATH_TO_COMPAS_RESULTS/sexAge/compas_sexAge_CFA_theta=1.csv
+
