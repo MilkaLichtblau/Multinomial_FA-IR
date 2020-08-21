e967565dc882b7559d25c086d63d12bad8978b2c
Meike Zehlike <meike.zehlike@tu-berlin.de>
2020-08-21 11:19:19 +0200

    bugfix

--- experiments/dataExperiments/startAllScriptsInParallel.sh
+++ experiments/dataExperiments/startAllScriptsInParallel.sh
@@ -4,33 +4,28 @@ module load gnu-parallel
 module load bwa
 module load samtools
 
-process-dir() {
   # Process all jobs for current folder, sequentially.
-  # Input: Folder, e.g. THAKID0001_dir
-	'compasAge-p1=01,p2=06.sh'
-	'compasAge-p1=02,p2=05.sh'
-	'compasAge-p1=03,p2=04.sh'
-  'compasAge-p1=04,p2=03.sh'
-	'compasAge-p1=05,p2=02.sh'
-  'compasAge-p1=06,p2=01.sh'
-   compasAge-baseline.sh       
-   compasAge-pAllEqual.sh    
-   compasAge-pStatisticalParity.sh
-   compasRace-baseline.sh  
-   compasRace.sh
-   compasWorstThree-baseline.sh 
-   compasWorstThree-pAllEqual.sh
-   compasWorstThree-pStatisticalParity.sh     
-   LSATSexRace-baseline.sh
-   LSATSexRace-pAllEqual.sh
-   LSATSexRace-pStatisticalParity.sh
-   germanCreditSexAge-baseline.sh
-   germanCreditSexAge-pAllEqual.sh
-   germanCreditSexAge-pInbetween.sh  
-   germanCreditSexAge-pStatisticalParity.sh
-}
+parallel -j 48 < 'compasAge-p1=01,p2=06.sh'
+parallel -j 48 < 'compasAge-p1=02,p2=05.sh'
+parallel -j 48 < 'compasAge-p1=03,p2=04.sh'
+parallel -j 48 < 'compasAge-p1=04,p2=03.sh'
+parallel -j 48 < 'compasAge-p1=05,p2=02.sh'
+parallel -j 48 < 'compasAge-p1=06,p2=01.sh'
+parallel -j 48 < compasAge-baseline.sh       
+parallel -j 48 < compasAge-pAllEqual.sh    
+parallel -j 48 < compasAge-pStatisticalParity.sh
+parallel -j 48 < compasRace-baseline.sh  
+parallel -j 48 < compasRace.sh
+parallel -j 48 < compasWorstThree-baseline.sh 
+parallel -j 48 < compasWorstThree-pAllEqual.sh
+parallel -j 48 < compasWorstThree-pStatisticalParity.sh     
+parallel -j 48 < LSATSexRace-baseline.sh
+parallel -j 48 < LSATSexRace-pAllEqual.sh
+parallel -j 48 < LSATSexRace-pStatisticalParity.sh
+parallel -j 48 < germanCreditSexAge-baseline.sh
+parallel -j 48 < germanCreditSexAge-pAllEqual.sh
+parallel -j 48 < germanCreditSexAge-pInbetween.sh  
+parallel -j 48 < germanCreditSexAge-pStatisticalParity.sh
 
-export -f process-dir
 
-parallel -j 48 process-dir
 
