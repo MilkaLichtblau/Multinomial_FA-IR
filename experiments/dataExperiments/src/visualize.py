da5ec98339e4d737287050fc70199eb907c884fa
Meike Zehlike <meike.zehlike@tu-berlin.de>
2020-09-29 17:47:46 +0200

    code for heatmap plots

--- experiments/dataExperiments/src/visualize.py
+++ experiments/dataExperiments/src/visualize.py
@@ -99,7 +99,7 @@ def plot3DFigure(xPos, yPos, zHeight, zLabel, filename):
     plt.savefig(filename, dpi=100, bbox_inches='tight')
 
 
-def prepare3DFigure(filenamePattern, kString):
+def prepareAndSaveFigures(filenamePattern, kString):
     allEvalFilenames = glob.glob(filenamePattern)
     data = []
     for filename in allEvalFilenames:
@@ -116,7 +116,11 @@ def prepare3DFigure(filenamePattern, kString):
 
     dataToPlot = pd.DataFrame(data, columns=["minProp1", "minProp2", "expGainGroup1", "expGainGroup2", "ndcgLoss"])
     dataToPlot.sort_values(["minProp1", "minProp2"], inplace=True)
-    print(dataToPlot)
+    # normalize exposure data
+    minGain = dataToPlot[["expGainGroup1", "expGainGroup2"]].min().min()
+    maxGain = dataToPlot[["expGainGroup1", "expGainGroup2"]].max().max()
+    dataToPlot["expGainGroup1"] = (dataToPlot["expGainGroup1"] - minGain) / (maxGain - minGain)
+    dataToPlot["expGainGroup2"] = (dataToPlot["expGainGroup2"] - minGain) / (maxGain - minGain)
 
     plot3DFigure(dataToPlot["minProp1"].values,
                  dataToPlot["minProp2"].values,
@@ -136,16 +140,184 @@ def prepare3DFigure(filenamePattern, kString):
                  "NDCG loss",
                  "../results/COMPAS/evalAndPlots/age/" + kString + "-barplot-ndcgLoss.png")
 
+    plotHeatmap(dataToPlot,
+                "expGainGroup1",
+                "Exposure Gain",
+                 "../results/COMPAS/evalAndPlots/age/" + kString + "-heatmap-expGainGroup1.png",
+                 valfmt="{x:.3f}")
+    plotHeatmap(dataToPlot,
+                "expGainGroup2",
+                "Exposure Gain",
+                 "../results/COMPAS/evalAndPlots/age/" + kString + "-heatmap-expGainGroup2.png",
+                 valfmt="{x:.3f}")
+    plotHeatmap(dataToPlot,
+                "ndcgLoss",
+                "NDCG Loss",
+                "../results/COMPAS/evalAndPlots/age/" + kString + "-heatmap-ndcgLoss.png",
+                valfmt="{x:.3f}")
+
+
+def plotHeatmap(dataToPlot, heatmapValueStr, cbarlabel, filename, **annotationKW):
+    heatmapFrame = pd.DataFrame(index=dataToPlot["minProp1"].unique(),
+                                columns=dataToPlot["minProp2"].unique(),
+                                dtype=np.float64)
+
+    for colName, colContent in heatmapFrame.iteritems():
+        for rowName in colContent.index:
+            val = dataToPlot.loc[((dataToPlot["minProp1"] == colName) & (dataToPlot["minProp2"] == rowName)), heatmapValueStr].values
+            if len(val) == 0:
+                heatmapFrame.at[rowName, colName] = np.nan
+            else:
+                heatmapFrame.at[rowName, colName] = val[0]
+
+    fig = plt.figure(figsize=(16, 16))
+    ax = fig.add_subplot(111)
+    im, _ = heatmap(heatmapFrame, heatmapFrame.index, heatmapFrame.columns, ax=ax,
+                    cmap="coolwarm", cbarlabel=cbarlabel, vmin=0, vmax=1)
+    annotate_heatmap(im, thresholds=[0.15, 0.85], **annotationKW)
+    plt.savefig(filename, dpi=100, bbox_inches='tight')
+
+
+def heatmap(data, row_labels, col_labels, ax=None,
+            cbar_kw={}, cbarlabel="", **kwargs):
+    """
+    Create a heatmap from a numpy array and two lists of labels.
+
+    Parameters
+    ----------
+    data
+        A 2D numpy array of shape (N, M).
+    row_labels
+        A list or array of length N with the labels for the rows.
+    col_labels
+        A list or array of length M with the labels for the columns.
+    ax
+        A `matplotlib.axes.Axes` instance to which the heatmap is plotted.  If
+        not provided, use current axes or create a new one.  Optional.
+    cbar_kw
+        A dictionary with arguments to `matplotlib.Figure.colorbar`.  Optional.
+    cbarlabel
+        The label for the colorbar.  Optional.
+    **kwargs
+        All other arguments are forwarded to `imshow`.
+    """
+
+    mpl.rcParams.update({'font.size':24, 'lines.linewidth':3, 'lines.markersize':15, 'font.family':'Times New Roman'})  # avoid type 3 (i.e. bitmap) fonts in figures
+    mpl.rcParams['ps.useafm'] = True
+    mpl.rcParams['pdf.use14corefonts'] = True
+    mpl.rcParams['text.usetex'] = True
+
+    if not ax:
+        ax = plt.gca()
+
+    # handle nans
+    current_cmap = mpl.cm.get_cmap()
+    current_cmap.set_bad(color='w')
+
+    # Plot the heatmap
+    im = ax.imshow(data, **kwargs)
+
+    # Create colorbar
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+
+    # We want to show all ticks...
+    ax.set_xticks(np.arange(data.shape[1]))
+    ax.set_yticks(np.arange(data.shape[0]))
+    # ... and label them with the respective list entries.
+    ax.set_xticklabels(col_labels)
+    ax.set_yticklabels(row_labels)
+
+    # Let the horizontal axes labeling appear on top.
+    # ax.tick_params(top=True, bottom=False,
+    #               labeltop=True, labelbottom=False)
+
+    # Rotate the tick labels and set their alignment.
+#     plt.setp(ax.get_xticklabels(), rotation=-30, ha="right",
+#              rotation_mode="anchor")
+
+    # Turn spines off and create white grid.
+    for _, spine in ax.spines.items():
+        spine.set_visible(False)
+
+    ax.set_xticks(np.arange(data.shape[1] + 1) - .5, minor=True)
+    ax.set_yticks(np.arange(data.shape[0] + 1) - .5, minor=True)
+    ax.grid(which="minor", color="w", linestyle='-', linewidth=3)
+    ax.tick_params(which="minor", bottom=False, left=False)
+
+    ax.invert_yaxis()
+
+    ax.set_xlabel('p\_{1}', labelpad=15)
+    ax.set_ylabel('p\_{2}', labelpad=15)
+
+    return im, cbar
+
+
+def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
+                     textcolors=["black", "white"],
+                     thresholds=[0, 1],
+                     ** textkw):
+    """
+    A function to annotate a heatmap.
+
+    Parameters
+    ----------
+    im
+        The AxesImage to be labeled.
+    data
+        Data used to annotate.  If None, the image's data is used.  Optional.
+    valfmt
+        The format of the annotations inside the heatmap.  This should either
+        use the string format method, e.g. "$ {x:.2f}", or be a
+        `matplotlib.ticker.Formatter`.  Optional.
+    textcolors
+        A list or array of two color specifications.  The first is used for
+        values below a threshold, the second for those above.  Optional.
+    threshold
+        Value in data units according to which the colors from textcolors are
+        applied.  If None (the default) uses the middle of the colormap as
+        separation.  Optional.
+    **kwargs
+        All other arguments are forwarded to each call to `text` used to create
+        the text labels.
+    """
+
+    if not isinstance(data, (list, np.ndarray)):
+        data = im.get_array()
+
+    # Set default alignment to center, but allow it to be
+    # overwritten by textkw.
+    kw = dict(horizontalalignment="center",
+              verticalalignment="center")
+    kw.update(textkw)
+
+    # Get the formatter in case a string is supplied
+    if isinstance(valfmt, str):
+        valfmt = mpl.ticker.StrMethodFormatter(valfmt)
+
+    # Loop over the data and create a `Text` for each "pixel".
+    # Change the text's color depending on the data.
+    texts = []
+
+    # have white text color in masked fields
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            if data[i, j] in data[data.mask]:
+                continue
+            kw.update(color=textcolors[int((thresholds[0] > data[i, j]) or (data[i, j] > thresholds[1]))])
+            text = im.axes.text(j, i, valfmt(data[i, j], None), **kw)
+            texts.append(text)
+
+    return texts
+
 
 def main():
 #     visualizeOrigCompasData()
 #     visualizeOrigGermanCreditData()
 #     visualizeOrigLSATData()
-    kString = "k=100"
-    prepare3DFigure("../results/COMPAS/evalAndPlots/age/" + kString + "*_multiFairResult.csv", kString)
 
     kString = "k=200"
-    prepare3DFigure("../results/COMPAS/evalAndPlots/age/" + kString + "*_multiFairResult.csv", kString)
+    prepareAndSaveFigures("../results/COMPAS/evalAndPlots/age/" + kString + "*_multiFairResult.csv", kString)
 
 
 if __name__ == '__main__':
